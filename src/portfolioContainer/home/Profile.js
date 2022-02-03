7839be0dab4f42c5b262fa51f7dd0b58ab0bf2f8
Bishal Kumar Singh <bishal.kumar@digitalshark.in>
2022-02-04 00:30:56 +0530

    changes

--- src/portfolioContainer/home/Profile.js
+++ src/portfolioContainer/home/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Typical from 'react-typical';
 import './Profile.css';
+import pdf from "../../assest/BishalKumaeSInghResume_dst.pdf"
 
 export default function Profile() {
     return (
@@ -10,10 +11,11 @@ export default function Profile() {
                     <div className="profile-details">
                         <div className="colz">
                             <div className="colz-icon">
-                                <a href="#"><i className="fa fa-facebook-square"></i></a>
+                                <a href="https://www.facebook.com/profile.php?id=100015864284283"><i className="fa fa-facebook-square"></i></a>
                                 <a href="#"><i className="fa fa-instagram"></i></a>
-                                <a href="#"><i className="fa fa-youtube"></i></a>
+                                <a href="https://www.youtube.com/channel/UCZGiKKFr6PhqGN9zkyd7yRg"><i className="fa fa-youtube"></i></a>
                                 <a href="#"><i className="fa fa-twitter"></i></a>
+                                <a href="https://github.com/Bishal123-art"><i class="fa fa-github"></i></a>
                             </div>
                         </div>
                         <div className="profile-details-name">
@@ -27,11 +29,11 @@ export default function Profile() {
                                 <h1 style={{ fontSize: "30px" }}>
                                     <Typical
                                         steps={[
-                                            'Full Stack Developer!', 1000,
-                                            'Frontend Developer!', 1000,
-                                            'Backend Developer!', 1000,
-                                            'MERN Stack Developer!', 1000,
-                                            'UI Designer!', 1000,
+                                            'Full Stack Developer !', 1000,
+                                            'Frontend Developer !', 1000,
+                                            'Backend Developer !', 1000,
+                                            'MSRN Stack Developer !', 1000,
+                                            'UI Designer !', 1000,
                                         ]}
                                         loop={Infinity}
                                         wrapper="p"
@@ -44,7 +46,7 @@ export default function Profile() {
                         </div>
                         <div className="profile-option">
                             <button className="hire-me">Hire Me</button>
-                            <a href="BishalKumaeSInghResume_dst.pdf" download="BishalKumaeSInghResume_dst.pdf">
+                            <a href={pdf} download="BishalKumaeSInghResume_dst.pdf">
                                 <button className="get-resume">Get Resume</button>
                             </a>
                         </div>
